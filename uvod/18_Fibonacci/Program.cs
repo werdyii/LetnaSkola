d8cea18687dd284a3e14dc554d73a2438e650d7b
Gusepe Werdy <gusepe.werdy@gmail.com>
2016-12-27 21:41:21 +0100

    Init uloha 18

---
+++ uvod/18_Fibonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LetnaSkola
+{
+    class FibonacciNumber
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
