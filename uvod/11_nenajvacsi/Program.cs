8207bd15e62550a87b67a492ab6292fb4062844a
Gusepe Werdy <gusepe.werdy@gmail.com>
2016-12-27 21:08:14 +0100

    Uloha 11 vyriešena

--- uvod/11_nenajvacsi/Program.cs
+++ uvod/11_nenajvacsi/Program.cs
@@ -6,8 +6,20 @@ namespace LetnaSkola
     {
         static void Main(string[] args)
         {
+            string sLine = Console.ReadLine();
+            var aLine = sLine.Split(' ', ',');
 
-            Console.ReadLine();
+            int max = int.Parse(aLine[0]);
+            int iSucet = 0;
+
+            for (int i = 0; i != aLine.Length; i++)
+            {
+                iSucet += int.Parse(aLine[i]);
+                if (int.Parse(aLine[i]) > max) max = int.Parse(aLine[i]);
+            }
+
+            Console.WriteLine(iSucet - max);
+            //Console.ReadLine();
         }
     }
 }
