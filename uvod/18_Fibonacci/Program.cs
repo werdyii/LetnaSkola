fb99f6feef8d6e765823e3fa04ef986c3b865080
Gusepe Werdy <gusepe.werdy@gmail.com>
2016-12-27 22:00:07 +0100

    Uloha 18 splnená

--- uvod/18_Fibonacci/Program.cs
+++ uvod/18_Fibonacci/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace LetnaSkola
 {
@@ -9,8 +7,54 @@ namespace LetnaSkola
     {
         static void Main(string[] args)
         {
+            string sLine = Console.ReadLine();
+            int n = int.Parse(sLine);
 
+            var generator = new FibonacciGenerator();
+            foreach (var digit in generator.Generate(n))
+            {
+                if (digit > 0) Console.Write(" ");
+                Console.Write(digit);
+            }
+            Console.WriteLine();
             Console.ReadLine();
         }
     }
+
+    public class FibonacciGenerator
+    {
+        private Dictionary<int, int> _cache = new Dictionary<int, int>();
+
+        //private int Fib(int n) => n < 2 ? n : FibValue(n - 1) + FibValue(n - 2);
+        private int Fib(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else {
+                return FibValue(n - 1) + FibValue(n - 2);
+            }
+        }
+
+        public int FibValue(int n)
+        {
+            if (!_cache.ContainsKey(n))
+            {
+                _cache.Add(n, Fib(n));
+                //Console.Write(n + " ");
+            }
+
+            return _cache[n];
+        }
+
+        public IEnumerable<int> Generate(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                yield return FibValue(i);
+            }
+        }
+    }
+
 }
