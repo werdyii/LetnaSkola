ff89c5fc81e12a432d0572e20458adc8ee63f834
Gusepe Werdy <gusepe.werdy@gmail.com>
2016-12-27 21:29:51 +0100

    Uloha 16 hotova

--- uvod/16_pyramida/Program.cs
+++ uvod/16_pyramida/Program.cs
@@ -9,8 +9,17 @@ namespace LetnaSkola
     {
         static void Main(string[] args)
         {
+            string sLine = Console.ReadLine();
+            int n = int.Parse(sLine);
 
-            Console.ReadLine();
+            for(int i = 1; i <= n ; i++)
+            {
+                for (int j = 0; j < (n - i); j++) Console.Write(" ");
+                for (int j = 0; j < ((2*i)-1); j++) Console.Write("*");
+                for (int j = 0; j < (n - i); j++) Console.Write(" ");
+                Console.WriteLine();
+            }
+            //Console.ReadLine();
         }
     }
 }
